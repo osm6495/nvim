fd564b9c7798621851717c08dcf21e38d3ff6091
Owen McCarthy <osm6495@rit.edu>
2023-08-24 16:46:22 -0400

    Changed nerd-tree to show hidden files

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -114,6 +114,18 @@ return {
       },
     },
     filesystem = {
+      filtered_items = {
+	      visible = true,
+	      show_hidden_count = true,
+	      hide_dotfiles = false,
+	      hide_gitignored = true,
+	      hide_by_name = {
+	        -- '.git',
+	        -- '.DS_Store',
+	        -- 'thumbs.db',
+	      },
+	      never_show = {},
+      },
       follow_current_file = true,
       hijack_netrw_behavior = "open_current",
       use_libuv_file_watcher = true,
